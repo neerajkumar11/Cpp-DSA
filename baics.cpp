011ed65d35b180993f2b16b82d23d2b3162ba2d8
neerajkumar11 <67398710+neerajkumar11@users.noreply.github.com>
2021-01-17 08:06:14 +0530

    17/Jan/2021

---
+++ baics.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    float a = 1.141;
+    float b  = 6.71;
+    float c = -7.951;
+    float d = -5.234;
+
+    cout<<floor(a)<<endl;
+    cout<<floor(b)<<endl;
+    cout<<floor(c)<<endl;
+    cout<<floor(d)<<endl;
+    cout<<endl;
+    cout<<ceil(a)<<endl;
+    cout<<ceil(b)<<endl;
+    cout<<ceil(c)<<endl;
+    cout<<ceil(d)<<endl;
+    cout<<endl;
+    cout<<trunc(a)<<endl;
+    cout<<trunc(b)<<endl;
+    cout<<trunc(c)<<endl;
+    cout<<trunc(d)<<endl;
+    cout<<endl;
+    cout<<round(a)<<endl;
+    cout<<round(b)<<endl;
+    cout<<round(c)<<endl;
+    cout<<round(d)<<endl;
+    cout<<endl;
+    cout<<fixed<<setprecision(0)<<a<<endl;
+    cout<<fixed<<setprecision(1)<<b<<endl;
+    cout<<fixed<<setprecision(3)<<c<<endl;
+    cout<<fixed<<setprecision(4)<<d<<endl;
+
+    return 0;
+}
